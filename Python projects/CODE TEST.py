389024156b4e094c831222267c9d811970c5a1e2
Bram Vicera <geofvicera@gmail.com>
2026-05-29 15:29:17 +0800

    Add discount calculation and media catalogue functionality

--- Python projects/CODE TEST.py
+++
@@ -1,7 +0,0 @@
-import pdb
-def add_setting(settings, new_setting):
-    pdb.set_trace()  # Set a breakpoint to debug the function
-    key = new_setting.lower()  # CRASH HERE
-    
-settings = {'theme': 'light'}
-add_setting(settings, ('THEME', 'dark'))
